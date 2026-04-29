200fabc2470ad01846ab1d0db9985210b663d720
Ollie24216 <24216or@hvhs.school.nz>
2026-04-29 02:44:16 +0000

    Apr 29, 2026, 2:44 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,18 @@
-/* Paste your code from task 2 here */
+/******************************
+ Name of task:
+ ******************************/
+
+//Variables
+let Username = "Ollie";
+let year = 2025;
+let age = 15;
+let money = 2;
+
+/******************************
+ Main code
+ ******************************/
+console.log("hi "+ Username + " as of " + age + " years old. You have " + money + "dollars ")
+
+/******************************
+ Functions
+ ******************************/
