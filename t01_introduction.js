8572e33dea6296257bc30a063dc308f319de2598
Ollie24216 <24216or@hvhs.school.nz>
2026-04-29 02:20:32 +0000

    Apr 29, 2026, 2:20 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,9 +1,22 @@
-/****************************
-This is a block comment.
-Put your header comment here!
-****************************/
+
+/******************************
+ Name of task:
+ ******************************/
+console.log( "Running T1-intro.js" );
+
+//Variables
+
+
+/******************************
+ Main code
+ ******************************/
+
+
+/******************************
+ Functions
+ ******************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log()
 
 // This is a single line comment
 
