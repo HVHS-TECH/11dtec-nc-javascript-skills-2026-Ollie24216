48534e9c3495864621bd814fbc10acfe8173266a
Ollie24216 <24216or@hvhs.school.nz>
2026-04-29 02:49:32 +0000

    Apr 29, 2026, 2:49 AM

--- t03_maths.js
+++ t03_maths.js
@@ -7,12 +7,16 @@ let Username = "Ollie";
 let year = 2025;
 let age = 15;
 let money = 2;
+let birthYear
+let oldAge
+oldAge = age + 10;
+birthYear = year - age;
 
 /******************************
  Main code
  ******************************/
-console.log("hi "+ Username + " as of " + age + " years old. You have " + money + "dollars ")
-
+console.log("hi "+ Username + " as of " + year + "you are" + age + " years old." + age + years old)
+console.log(" you were born in " + birthYear)
 /******************************
  Functions
- ******************************/
+ ******************************
