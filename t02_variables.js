394969c624b4d193de74caab2db026aca25c7a1b
Ollie24216 <24216or@hvhs.school.nz>
2026-04-29 02:39:21 +0000

    Apr 29, 2026, 2:39 AM

--- t02_variables.js
+++ t02_variables.js
@@ -5,13 +5,13 @@
 //Variables
 let Username = "Ollie";
 let year = 2025;
-let age = 28;
+let age = 15;
 let money = 2;
 
 /******************************
  Main code
  ******************************/
-console.log("hi "+ Username + " as of " +  + " years old. You have " + money + "dollars ")
+console.log("hi "+ Username + " as of " + age + " years old. You have " + money + "dollars ")
 
 /******************************
  Functions
