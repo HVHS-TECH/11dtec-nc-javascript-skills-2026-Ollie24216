2a7db195f9e2cbd06b597af747a0dfe1ea50e26b
Ollie24216 <24216or@hvhs.school.nz>
2026-05-03 22:43:41 +0000

    May 3, 2026, 10:43 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,31 @@
 /* Paste your code from task 3, Maths here */
+/******************************
+ Name of task:
+ ******************************/
+
+//Variables
+let Username = "Ollie";
+let year = 2025;
+let age = 15;
+let money = 2;
+let birthYear
+let oldAge
+oldAge = age + 10;
+birthYear = year - age;
+halfMoney = money / 2;
+
+/******************************
+ Main code
+ 
+ ******************************/
+OUTPUT.innerHTML += " <p><p/> hi "+ Username + " as of " + year + " you are " + age + " years old. " + age + " years old <br>"
+OUTPUT.innerHTML += "you were born in " + birthYear + "<br>"
+OUTPUT.innerHTML += "in 10 years you will be " + oldAge + " years old <br>"
+OUTPUT.innerHTML += "
+OUTPUT.innerHTML += "
+OUTPUT.innerHTML += "
+
+
+
+
+
