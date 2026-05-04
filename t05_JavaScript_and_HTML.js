5015ad0178e8e8230a185a8609b72e6813337a96
Ollie24216 <24216or@hvhs.school.nz>
2026-05-04 23:44:01 +0000

    May 4, 2026, 11:44 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,28 +1,30 @@
-
-/******************************
- Name of task:
- ******************************/
-let OUTPUT = document.getElementById("output");
-//Variables
+/*****************************/
+// Variables
 let Username = "Ollie";
 let year = 2025;
 let age = 15;
 let money = 2;
-let birthYear
-let oldAge
+
+let birthYear;
+let oldAge;
+let halfMoney;
+
 oldAge = age + 10;
 birthYear = year - age;
 halfMoney = money / 2;
 
-/******************************
- Main code
- 
- ******************************/
-OUTPUT.innerHTML += ("<p>" + "hi " + Username + " as of " + year + " you are " + age + " years old." + "</p>")
-OUTPUT.innerHTML += ("<p>" + "you were born in " + birthYear + "</p>")
-OUTPUT.innerHTML += ("<p>" + "in 10 years you will be " + oldAge + "</p>")
-OUTPUT.innerHTML += ("<p>" + "you have " + halfMoney + " pounds" + "</p>")
-OUTPUT.innerHTML += ("<p>" + "you have " + money + " pounds" + "</p>")
+
+let OUTPUT = document.getElementById("output");
+
+/*****************************
+Main code
+*****************************/
+
+OUTPUT.innerHTML += "<p>hi " + Username + " as of " + year + " you are " + age + " years old.</p>";
+OUTPUT.innerHTML += "<p>you were born in " + birthYear + "</p>";
+OUTPUT.innerHTML += "<p>in 10 years you will be " + oldAge + "</p>";
+OUTPUT.innerHTML += "<p>you have " + halfMoney + " pounds</p>";
+OUTPUT.innerHTML += "<p>you have " + money + " pounds</p>";
 
 /******************************
  Functions
