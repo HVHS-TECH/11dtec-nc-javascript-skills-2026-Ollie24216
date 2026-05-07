7ae1a71dd21cf1532c7dce99d5df969a9208d642
Ollie24216 <24216or@hvhs.school.nz>
2026-05-07 21:41:58 +0000

    May 7, 2026, 9:41 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -43,3 +43,8 @@ displayWelcome("Ms Alice", 16);
   displayWelcome("Mr Bob", 99);
 
   <button onClick="start()">START</button>
+  
+  <p>Open the console <code>(Ctrl-shift-i)</code></p>
+<button onclick="start()">Start</button>
+
+<div id="spaceForJavaScriptOutput"></div>
