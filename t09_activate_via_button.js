766c4d0caccd17ba3cf20ad13099224360f39d2d
Ollie24216 <24216or@hvhs.school.nz>
2026-05-25 23:53:51 +0000

    May 25, 2026, 11:53 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -45,3 +45,14 @@ function start() {
 
     spaceForJavaScriptOutput.innerHTML
 }
+
+ function Start() {
+    // Add a line to the html page
+    OUTPUT.innerHTML += "<p>Less code matters</p>";
+}
+function displayWelcome(_name, _age){
+  OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+  OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+displayWelcome("Ms Alice", 16);
+  displayWelcome("Mr Bob", 99);
