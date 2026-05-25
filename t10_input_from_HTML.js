01de4c538cbac9f697a4cdc60f3dddefbbc965de
Ollie24216 <24216or@hvhs.school.nz>
2026-05-25 23:36:07 +0000

    May 25, 2026, 11:36 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,37 @@
 /* Paste your code from the last task */
+/*****************************/
+// Variables
+
+let Username = "Ollie";
+let year = 2025;
+let age = 15;
+let money = 2;
+
+let birthYear;
+let oldAge;
+let halfMoney;
+
+oldAge = age + 10;
+birthYear = year - age;
+halfMoney = money / 2;
+
+let OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+/*****************************
+ Main code
+*****************************/
+
+
+OUTPUT.innerHTML += "<p>Hi " + Username + ", as of " + year + " you are " + age + " years old.</p>";
+
+OUTPUT.innerHTML += "<p>You were born in " + birthYear + ".</p>";
+
+OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + ".</p>";
+
+OUTPUT.innerHTML += "<p>You have " + money + " pounds.</p>";
+
+OUTPUT.innerHTML += "<p>Half your money is " + halfMoney + " pounds.</p>";
+
+/******************************
+ Functions
+******************************/
