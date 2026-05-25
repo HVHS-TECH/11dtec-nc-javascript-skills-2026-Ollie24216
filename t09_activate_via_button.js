700647e5f5ad5a400008f3170cdcd115bc74f4cc
Ollie24216 <24216or@hvhs.school.nz>
2026-05-25 23:55:11 +0000

    May 25, 2026, 11:55 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -21,15 +21,7 @@ let OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 *****************************/
 
 
-OUTPUT.innerHTML += "<p>Hi " + Username + ", as of " + year + " you are " + age + " years old.</p>";
 
-OUTPUT.innerHTML += "<p>You were born in " + birthYear + ".</p>";
-
-OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + ".</p>";
-
-OUTPUT.innerHTML += "<p>You have " + money + " pounds.</p>";
-
-OUTPUT.innerHTML += "<p>Half your money is " + halfMoney + " pounds.</p>";
 
 /******************************
  Functions
@@ -47,7 +39,15 @@ function start() {
 }
 
  function start() {
-   
+   OUTPUT.innerHTML += "<p>Hi " + Username + ", as of " + year + " you are " + age + " years old.</p>";
+
+OUTPUT.innerHTML += "<p>You were born in " + birthYear + ".</p>";
+
+OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + ".</p>";
+
+OUTPUT.innerHTML += "<p>You have " + money + " pounds.</p>";
+
+OUTPUT.innerHTML += "<p>Half your money is " + halfMoney + " pounds.</p>";
 }
 function displayWelcome(_name, _age){
   OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
