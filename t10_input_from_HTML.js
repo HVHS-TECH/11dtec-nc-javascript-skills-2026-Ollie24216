94dcfab27644d792587d42385c248925fbf24ea1
Ollie24216 <24216or@hvhs.school.nz>
2026-05-25 23:58:38 +0000

    May 25, 2026, 11:58 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,4 +1,3 @@
-/* Paste your code from the last task */
 /*****************************/
 // Variables
 
@@ -22,7 +21,25 @@ let OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 *****************************/
 
 
-OUTPUT.innerHTML += "<p>Hi " + Username + ", as of " + year + " you are " + age + " years old.</p>";
+
+
+/******************************
+ Functions
+******************************/
+
+
+function start() {
+
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+
+    answer = currentYear - userAge;
+
+    spaceForJavaScriptOutput.innerHTML
+}
+
+ function start() {
+   OUTPUT.innerHTML += "<p>Hi " + Username + ", as of " + year + " you are " + age + " years old.</p>";
 
 OUTPUT.innerHTML += "<p>You were born in " + birthYear + ".</p>";
 
@@ -31,7 +48,10 @@ OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + ".</p>";
 OUTPUT.innerHTML += "<p>You have " + money + " pounds.</p>";
 
 OUTPUT.innerHTML += "<p>Half your money is " + halfMoney + " pounds.</p>";
-
-/******************************
- Functions
-******************************/
+}
+function displayWelcome(_name, _age){
+  OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+  OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+displayWelcome("Ms Alice", 16);
+  displayWelcome("Mr Bob", 99);
