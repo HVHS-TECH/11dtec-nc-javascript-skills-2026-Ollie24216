e8fa8a8f38e04c2767a407dd1e2c97770ef35e4d
Ollie24216 <24216or@hvhs.school.nz>
2026-05-26 00:06:04 +0000

    May 26, 2026, 12:06 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,1 +1,59 @@
 /* Paste your code from the last task */
+/*****************************/
+// Variables
+
+let Username = "Ollie";
+let year = 2025;
+let age = 15;
+let money = 2;
+
+let birthYear;
+let oldAge;
+let halfMoney;
+
+oldAge = age + 10;
+birthYear = year - age;
+halfMoney = money / 2;
+
+let OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+/*****************************
+ Main code
+*****************************/
+
+
+
+
+/******************************
+ Functions
+******************************/
+
+
+function start() {
+
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+
+    answer = currentYear - userAge;
+
+    spaceForJavaScriptOutput.innerHTML
+}
+
+ function start() {
+   OUTPUT.innerHTML += "<p>Hi " + Username + ", as of " + year + " you are " + age + " years old.</p>";
+
+OUTPUT.innerHTML += "<p>You were born in " + birthYear + ".</p>";
+
+OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + ".</p>";
+
+OUTPUT.innerHTML += "<p>You have " + money + " pounds.</p>";
+
+OUTPUT.innerHTML += "<p>Half your money is " + halfMoney + " pounds.</p>";
+}
+function displayWelcome(_name, _age){
+  OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+  OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+displayWelcome("Ms Alice", 16);
+  displayWelcome("Mr Bob", 99);
+  
