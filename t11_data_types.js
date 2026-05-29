cf8014648f34827224c40eb34a817d148836c261
Ollie24216 <24216or@hvhs.school.nz>
2026-05-29 03:23:18 +0000

    May 29, 2026, 3:23 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,70 +1,48 @@
-/* Paste your code from the last task */
-/*****************************/
 // Variables
-
 let Username = "Ollie";
 let year = 2025;
 let age = 15;
 let money = 2;
 
-let birthYear;
-let oldAge;
-let halfMoney;
-
-oldAge = age + 10;
-birthYear = year - age;
-halfMoney = money / 2;
+let birthYear = year - age;
+let oldAge = age + 10;
+let halfMoney = money / 2;
 
 let OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
-var num1 = “50”;
-typeof(num1);
-
-let name = "Ms Alice";
-let age = 16;
-let isAwake = true;
-let userAgeString = AGE_FIELD.value;
-let userAgeNumber = Number(userAgeString);
-
-
-/*****************************
- Main code
-*****************************/
-
-
-
-
-/******************************
- Functions
-******************************/
-
-
-
+// Main function
 function start() {
 
-    spaceForJavaScriptOutput.innerHTML
-    spaceForJavaScriptOutput.innerHTML
+  OUTPUT.innerHTML += "<p>Hi " + Username + ", as of " + year + " you are " + age + " years old.</p>";
 
-    answer = currentYear - userAge;
+  OUTPUT.innerHTML += "<p>You were born in " + birthYear + ".</p>";
 
-    spaceForJavaScriptOutput.innerHTML
+  OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + ".</p>";
+
+  OUTPUT.innerHTML += "<p>You have " + money + " pounds.</p>";
+
+  OUTPUT.innerHTML += "<p>Half your money is " + halfMoney + " pounds.</p>";
 }
 
- function start() {
-   OUTPUT.innerHTML += "<p>Hi " + Username + ", as of " + year + " you are " + age + " years old.</p>";
-
-OUTPUT.innerHTML += "<p>You were born in " + birthYear + ".</p>";
-
-OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + ".</p>";
-
-OUTPUT.innerHTML += "<p>You have " + money + " pounds.</p>";
-
-OUTPUT.innerHTML += "<p>Half your money is " + halfMoney + " pounds.</p>";
+// Function with parameters
+function displayWelcome(_name, _age) {
+  OUTPUT.innerHTML += "<p>Welcome to the page " + _name + "</p>";
+  OUTPUT.innerHTML += "<p>You are " + _age + " years old</p>";
 }
-function displayWelcome(_name, _age){
-  OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
-  OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+
+// Form input function
+function getFormInput() {
+
+  let name = document.getElementById("nameField").value;
+  let age = document.getElementById("ageField").value;
+
+  OUTPUT.innerHTML += "<p>Hello " + name + "</p>";
+  OUTPUT.innerHTML += "<p>You are " + age + " years old</p>";
 }
+
+// Run functions
+start();
+
 displayWelcome("Ms Alice", 16);
-  displayWelcome("Mr Bob", 99);
+displayWelcome("Mr Bob", 99);
   
