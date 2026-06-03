8803d40f6e8b2b80fa71681e715989a6eaa53a23
Ollie24216 <24216or@hvhs.school.nz>
2026-06-03 09:31:57 +0000

    Jun 3, 2026, 9:31 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -13,7 +13,7 @@ let OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
 // Main function
 function start() {
-
+  // You can call your functions here when the page loads
 }
 
 // Function with parameters
@@ -22,21 +22,23 @@ function displayWelcome(_name, _money) {
   OUTPUT.innerHTML += "<p>You have $" + _money + " in pocket money</p>";
 }
 
-// Form input function
+// Single, combined Form input function
 function getFormInput() {
-
+  // 1. Get the values from the input fields
   let name = document.getElementById("nameField").value;
-  let pocketmoney = Number(document.getElementById("pocketmoneyField").value);
+  
+  // 2. Use Number() to convert the text input into a real number
+  let enteredPocketmoney = Number(document.getElementById("pocketmoneyField").value);
 
+  // 3. Print the standard welcome messages
   OUTPUT.innerHTML += "<p>Hello " + name + "</p>";
-  OUTPUT.innerHTML += "<p>You have " + enteredpocketmoney + " money </p>";
-  //OUTPUT.innerHTML += "<p>You have money </p>";
+  OUTPUT.innerHTML += "<p>You have $" + enteredPocketmoney + " money </p>";
+
+  // 4. The conditional check using the converted number
+  if (enteredPocketmoney >= 4) {
+    OUTPUT.innerHTML += "<p>You can afford a chocolate bar!</p>";
+  } else {
+    OUTPUT.innerHTML += "<p>You do not have enough money for a chocolate bar.</p>";
+  }
 }
 
-//if ("money" < 4) {
-//  OUTPUT.innerHTML += "<p>You can afford a chocolate bar! </p>";
-//}
-
-if (enteredpocketmoney >= 4) {
-OUTPUT.innerHTML += "<p>You have money </p>";
-}
