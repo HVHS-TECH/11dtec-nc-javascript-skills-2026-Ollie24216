3b96f106da7f59fc7fa29d72bc99985e76787eb3
Ollie24216 <24216or@hvhs.school.nz>
2026-06-03 02:36:08 +0000

    Jun 3, 2026, 2:36 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -44,8 +44,11 @@ function getFormInput() {
   OUTPUT.innerHTML += "<p>You are " + age + " years old</p>";
 }
 
-function pocketMoney() {
+function addPocketMoney() {
 
-  OUTPUT.innerHTML += "<p>You have " + money + " pounds of pocket money.</p>";
+  let extraMoney = Number(document.getElementById("moneyField").value);
 
-}
+  money = money + extraMoney;
+
+  OUTPUT.innerHTML += "<p>You received " + extraMoney + " pounds.</p>";
+  OUTPUT.innerHTML += "<p>You now have " + money + " pounds.</p>";
