515b360c6723cae91a8af1b85e82c8e86729d3ea
Ollie24216 <24216or@hvhs.school.nz>
2026-06-27 07:55:34 +0000

    Jun 27, 2026, 7:55 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -1,1 +1,37 @@
-/* Paste your code from the last task */
+let Username = "Ollie";
+
+let pocketmoney = 3;
+let money = 12;
+
+let price = 4;
+let OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+function start() { }
+
+
+
+function calculateChange(enteredPocketmoney, price) {
+    let chocPrice = 4;
+    let change = enteredPocketmoney - price;
+    return change;
+}
+
+function getFormInput() {
+    let name = document.getElementById("nameField").value;
+    let enteredPocketmoney = Number(document.getElementById("pocketmoneyField").value);
+
+   
+
+    let change = calculateChange(enteredPocketmoney, price);
+
+    OUTPUT.innerHTML += "<p>Hello " + name + "</p>";
+    OUTPUT.innerHTML += "<p>You have $" + enteredPocketmoney + " money </p>";
+    OUTPUT.innerHTML += "<p>You have $" + change + " in change after buying a chocolate bar </p>";
+
+    if (enteredPocketmoney >= price) {
+        OUTPUT.innerHTML += "<p>You can buy a chocolate bar!</p>";
+        OUTPUT.innerHTML += "<p>You will get $" + change.toFixed(2) + " change</p>";
+    } else {
+        OUTPUT.innerHTML += "<p>You can't afford the chocolate bar.</p>";
+    }
+}
