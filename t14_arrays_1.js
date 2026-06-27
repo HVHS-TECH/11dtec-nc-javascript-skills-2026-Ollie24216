fa0e5b6e83deddc5082c1558d2da56735f8742c1
Ollie24216 <24216or@hvhs.school.nz>
2026-06-27 08:17:17 +0000

    Jun 27, 2026, 8:17 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -7,15 +7,16 @@ function getFormInput() {
     let name = document.getElementById("nameField").value;
     let enteredChocrating = Number(document.getElementById("chocratingField").value);
 
-   let rating = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"]
+   let ratingArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"]
 
     let change = calculateChange(enteredPocketmoney, price);
 
     OUTPUT.innerHTML += "<p>Hello " + name + "</p>";
     OUTPUT.innerHTML += "<p>You have $" + enteredChocrating + " rating </p>";
     OUTPUT.innerHTML += "<p>You have $" + change + " in change after buying a chocolate bar </p>";
+    OUTPUT.innerHTML += "<p>ratingArray</p>";         
 
-    if (enteredPocketmoney >= price) {
+    if (enteredChocrating >= price) {
         OUTPUT.innerHTML += "<p>You can buy a chocolate bar!</p>";
         OUTPUT.innerHTML += "<p>You will get $" + change.toFixed(2) + " change</p>";
     } else {
