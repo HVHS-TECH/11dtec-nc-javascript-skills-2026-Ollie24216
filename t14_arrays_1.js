00d5f7c523e8105429d84094a62ccbff1227f60b
Ollie24216 <24216or@hvhs.school.nz>
2026-06-27 08:20:46 +0000

    Jun 27, 2026, 8:20 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -11,15 +11,9 @@ function getFormInput() {
 
 
 
-    OUTPUT.innerHTML += "<p>Hello " + name + "</p>";
-    OUTPUT.innerHTML += "<p>You have $" + enteredChocrating + " rating </p>";
-    OUTPUT.innerHTML += "<p>You have $" + change + " in change after buying a chocolate bar </p>";
-    OUTPUT.innerHTML += "<p>+ratingArray[2]</p>";         
+   
+    
+    OUTPUT.innerHTML += "<p>+ratingArray[enteredChocrating]</p>";         
 
-    if (enteredChocrating >= price) {
-        OUTPUT.innerHTML += "<p>You can buy a chocolate bar!</p>";
-        OUTPUT.innerHTML += "<p>You will get $" + change.toFixed(2) + " change</p>";
-    } else {
-        OUTPUT.innerHTML += "<p>You can't afford the chocolate bar.</p>";
     }
 }
