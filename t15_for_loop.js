40f1879ac138957789fcdb866fbb90270fe5704a
Ollie24216 <24216or@hvhs.school.nz>
2026-06-28 22:19:07 +0000

    Jun 28, 2026, 10:19 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,1 +1,16 @@
-/* Paste your code from the last task */
+
+function start() { }
+
+
+
+function getFormInput() {
+
+    let enteredChocrating = Number(document.getElementById("chocratingField").value);
+
+   let ratingArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"]
+
+    
+    OUTPUT.innerHTML += "<p>" + ratingArray[enteredChocrating] + "</p>";         
+
+    
+}
