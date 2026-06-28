3f3de4f5755054c218822c645e31bb9b18bff4d5
Ollie24216 <24216or@hvhs.school.nz>
2026-06-28 22:44:40 +0000

    Jun 28, 2026, 10:44 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -12,5 +12,9 @@ function getFormInput() {
     
     OUTPUT.innerHTML += "<p>" + ratingArray[enteredChocrating] + "</p>";         
 
+
+    for (let i = 0; i <= 30; i = i + 3) {
+    console.log("Current count: " + i);
+}
     
 }
