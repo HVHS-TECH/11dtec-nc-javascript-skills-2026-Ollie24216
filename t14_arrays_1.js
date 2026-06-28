01f35a425fa018ec53882b075553873d7142b149
Ollie24216 <24216or@hvhs.school.nz>
2026-06-28 06:03:02 +0000

    Jun 28, 2026, 6:03 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -4,7 +4,7 @@ function start() { }
 
 
 function getFormInput() {
-    let name = document.getElementById("nameField").value;
+
     let enteredChocrating = Number(document.getElementById("chocratingField").value);
 
    let ratingArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"]
