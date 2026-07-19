be0b8fca2f1501aa2625f6180a69eba1016eb6d0
Ollie24216 <24216or@hvhs.school.nz>
2026-07-19 08:13:19 +0000

    Jul 19, 2026, 8:13 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -6,13 +6,13 @@ function start() { }
 function getFormInput() {
 
     let BottleNumber = Number(document.getElementById("BottleNumberField").value);   
-    let OUTPUT = document.getElementById("output")
+    let OUTPUT = document.getElementById("OUTPUT")
     OUTPUT.innerHTML = ""; 
 
 
     for (let i = BottleNumber; i > 0; i--) {
-    console.log("Current count: + i")
-        OUTPUT.innerHTML += "<p>" + "There are" + i + "bottles of beer on the wall" + "</p>";         
+    console.log("Current count: " + i)
+        OUTPUT.innerHTML += "<p>" + "There are " + i + " bottles of beer on the wall" + "</p>";         
 ;
 
     }
