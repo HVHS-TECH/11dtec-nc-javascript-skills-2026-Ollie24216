2ae3877d80f6ef9e5f42de11bd40e79bac5f2a44
Ollie24216 <24216or@hvhs.school.nz>
2026-07-19 08:33:22 +0000

    Jul 19, 2026, 8:33 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -7,10 +7,12 @@ function getFormInput() {
 
     let Item = Number(document.getElementById("ShoppingItem").value);
 
-   let ratingArray = ["MILK", "Chocolate", "sausages", "CARROTS"]
+   let ItemArray = ["MILK", "Chocolate", "sausages", "CARROTS"]
+
+
 
     
-    OUTPUT.innerHTML += "<p>" + ratingArray[enteredChocrating] + "</p>";         
+    OUTPUT.innerHTML += "<p>" + ItemArray[all items] + "</p>";         
 
     
 }
