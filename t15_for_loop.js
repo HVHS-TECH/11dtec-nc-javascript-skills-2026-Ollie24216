45b5f556e6f4aa1fd641e88e9a6632f575d17525
Ollie24216 <24216or@hvhs.school.nz>
2026-07-19 08:16:17 +0000

    Jul 19, 2026, 8:16 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -12,6 +12,7 @@ function getFormInput() {
 
     for (let i = BottleNumber; i > 0; i--) {
     console.log("Current count: " + i)
+    let BottleWord = (i === 1) ? "bottle" : "bottles";
         OUTPUT.innerHTML += "<p>" + "There are " + i + " bottles of beer on the wall" + "</p>";         
 ;
 
