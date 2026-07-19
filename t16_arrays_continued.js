71561b2ecd439d54e3783eda10390f03d3f1660e
Ollie24216 <24216or@hvhs.school.nz>
2026-07-19 08:20:52 +0000

    Jul 19, 2026, 8:20 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -1,1 +1,20 @@
-/* Paste your code from the last task */
+
+function start() { }
+
+
+
+function getFormInput() {
+
+    let BottleNumber = Number(document.getElementById("BottleNumberField").value);   
+    let OUTPUT = document.getElementById("OUTPUT")
+    OUTPUT.innerHTML = ""; 
+
+
+    for (let i = BottleNumber; i > 0; i--) {
+    console.log("Current count: " + i)
+    let BottleWord = (i === 1) ? "bottle" : "bottles";
+        OUTPUT.innerHTML += "<p>" + "There are " + i + " " + BottleWord + " of beer on the wall" + "</p>";         
+;
+
+    }
+}
