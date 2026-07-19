aeb9c5533fe2c1614ceb2f7dbdc4fc2941b0d388
Ollie24216 <24216or@hvhs.school.nz>
2026-07-19 07:47:37 +0000

    Jul 19, 2026, 7:47 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -5,11 +5,7 @@ function start() { }
 
 function getFormInput() {
 
-    let enteredChocrating = Number(document.getElementById("chocratingField").value);
-
-   let ratingArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"]
-
-    
+    let BottleNumber = Number(document.getElementById("BottleNumberField").value);   
     OUTPUT.innerHTML += "<p>" + ratingArray[enteredChocrating] + "</p>";         
 
 
